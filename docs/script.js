941c2c2b269d100390bb3e44bcaebed9f97c3b06
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-06-10 23:44:11 -0500

    Updated index.html

--- docs/script.js
+++ docs/script.js
@@ -42,6 +42,48 @@ function test() {
         document.getElementById("tools-for-you").innerHTML = toolsArray.join(`<br>`);
       });
   }
+function loadChecking() {
+ let href = window.location.href;
+ if(!href.includes(atob("YWxvbnNvYWxpYWdhLmdpdGh1Yi5pbw=="))) return;
+ let link = atob("aHR0cHM6Ly9hbG9uc29hcGkuZGlzY2xvdWQuYXBwL2NoZWNraW5nP3NpdGU9PHNpdGU+JmtleT08a2V5Pg==")
+  .replace(/<site>/g,"yaml-parser").replace(/<key>/g,"KEY-A");
+ let counter = document.getElementById("online-counter");
+ if(counter) {
+   $.ajax({
+     url: link,
+     type: "GET", /* or type:"GET" or type:"PUT" */
+     dataType: "json",
+     data: {
+     },
+     success: function (result) {
+        //console.log(`Total fails: ${counter.dataset.failed}`)
+        counter.dataset.failed = "0";
+        counter.style.display = "flex";
+        if(isNaN(result)) {
+         counter.textContent = `🟡 You shouldn't be reading this. Report it on https://alonsoaliaga.com/discord`;
+         counter.style.backgroundColor = "yellow";
+        }else{
+         //counter.textContent = `🟢 ${result} user${result==1?``:`s`} online using our Minecraft Profile Picture Generator!`;
+         counter.textContent = `🟢 ${result} online using our YAML Parser to fix config issues!`;
+         counter.style.backgroundColor = "green";
+        }
+     },
+     error: function (e) {
+      //console.log(`Total fails: ${counter.dataset.failed}`)
+      if(counter.style.display != "none") {
+        let currentFails = +counter.dataset.failed;
+        if(currentFails >= 1){
+          counter.style.display = "none"
+        }else{
+          counter.textContent = `🔴 Check your internet connection!`;
+          counter.style.backgroundColor = "#7c0000";
+          counter.dataset.failed = `${currentFails + 1}`
+        }
+      }
+     }
+   });
+ }
+}
   let times = 0;
   function loadCounter() {
    let link = atob("aHR0cHM6Ly9hbG9uc29hcGkuZGlzY2xvdWQuYXBwL2NvdW50ZXI/c2l0ZT08c2l0ZT4ma2V5PTxrZXk+").replace(/<site>/g,"yaml-parser").replace(/<key>/g,"KEY-A");
@@ -128,6 +170,12 @@ function test() {
     forceParser();
     loadCounter();
     checkSite(window);
+    setTimeout(()=>{
+      loadChecking();
+      setInterval(()=>{
+        loadChecking();
+      },10000)
+    },2500)
   });
   
 function processAds() {
